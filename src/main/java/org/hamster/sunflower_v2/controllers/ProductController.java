bc3a72f70849ee3c495b9be2bd6efef3f1f05d99
Czeide Dydriene Avanzado <czeide_avanzado@dlsu.edu.ph>
2018-02-28 10:44:48 +0800

    PostMapping for selling products
    Added DTO for products

--- src/main/java/org/hamster/sunflower_v2/controllers/ProductController.java
+++ src/main/java/org/hamster/sunflower_v2/controllers/ProductController.java
@@ -1,7 +1,35 @@
 package org.hamster.sunflower_v2.controllers;
 
+import org.hamster.sunflower_v2.domain.models.ProductDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.WebRequest;
+
+import javax.validation.Valid;
+
 /**
  * Created by ONB-CZEIDE on 02/28/2018
  */
+@Controller
+@RequestMapping(value = "product")
 public class ProductController {
+
+    @GetMapping(value = "/sell")
+    public String sellProductForm(WebRequest request, Model model) {
+        ProductDTO product = new ProductDTO();
+        model.addAttribute("product", product);
+        return "registration";
+    }
+
+    @PostMapping(value = "/sell")
+    public String sellProduct(@ModelAttribute("product") @Valid ProductDTO productDTO, BindingResult result,
+                              WebRequest request, Errors errors) {
+        return "";
+    }
 }
---
+++ src/main/java/org/hamster/sunflower_v2/domain/models/ProductDTO.java
@@ -0,0 +1,58 @@
+package org.hamster.sunflower_v2.domain.models;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+/**
+ * Created by ONB-CZEIDE on 02/28/2018
+ */
+public class ProductDTO {
+
+    @NotNull
+    @NotEmpty
+    private String name;
+
+    @NotNull
+    @NotEmpty
+    private BigDecimal price;
+
+    private String description;
+    private String photo;
+
+    public ProductDTO() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+}
