35def798fdcd8e14922fbd0e24d2047aaac3404c
Czeide Dydriene Avanzado <czeide_avanzado@dlsu.edu.ph>
2018-02-28 10:14:56 +0800

    Added order, orderDetails, transaction models

---
+++ src/main/java/org/hamster/sunflower_v2/domain/models/Order.java
@@ -0,0 +1,16 @@
+package org.hamster.sunflower_v2.domain.models;
+
+import javax.persistence.*;
+
+/**
+ * Created by ONB-CZEIDE on 02/28/2018
+ */
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+}
---
+++ src/main/java/org/hamster/sunflower_v2/domain/models/Transaction.java
@@ -0,0 +1,12 @@
+package org.hamster.sunflower_v2.domain.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by ONB-CZEIDE on 02/28/2018
+ */
+@Entity
+@Table(name = "transactions")
+public class Transaction {
+}
