0bbba74f07b45e596107e7c64866883776371608
cavanzado <czeideavanzado@yahoo.com>
2018-04-12 10:02:16 +0800

    Error Controller

---
+++ src/main/java/org/hamster/sunflower_v2/controllers/CustomErrorController.java
@@ -0,0 +1,35 @@
+package org.hamster.sunflower_v2.controllers;
+
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by ONB-CZEIDE on 03/07/2018
+ */
+@Controller
+//@RequestMapping("/error")
+public class CustomErrorController implements ErrorController {
+
+    public static String ERROR_PATH = "errors/";
+
+    @RequestMapping(value = "/error")
+    String error(HttpServletRequest request, HttpServletResponse response) {
+        // Appropriate HTTP response code (e.g. 404 or 500) is automatically set by Spring.
+        // Here we just define response body.
+        if (response.getStatus() == 403) {
+            return "errors/403";
+        }
+
+        return "";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+}
--- src/main/java/org/hamster/sunflower_v2/exceptions/GlobalExceptionHandler.java
+++ src/main/java/org/hamster/sunflower_v2/exceptions/GlobalExceptionHandler.java
@@ -1,10 +1,6 @@
 package org.hamster.sunflower_v2.exceptions;
 
-import org.hamster.sunflower_v2.domain.models.User;
-import org.hamster.sunflower_v2.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -12,26 +8,19 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.ConcurrentModificationException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
 //    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
-    @Autowired
-    private UserService userService;
 
-//    @PreAuthorize("hasRole('ADMIN')")
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler(Exception.class)
+    @ExceptionHandler(ConcurrentModificationException.class)
     public void handleInternalServer(HttpServletResponse response, Exception e) {
-//        log.error("Unhandled exception occurred", e);
         try {
             Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
@@ -49,16 +38,6 @@ public class GlobalExceptionHandler {
             }
 
             response.sendRedirect("/");
-        } catch (IOException e1) {
-            e1.printStackTrace();
-        }
-    }
-
-    @ResponseStatus(HttpStatus.FORBIDDEN)
-    @ExceptionHandler(Exception.class)
-    public void handleForbiddenAccess(HttpServletResponse response, Exception e) {
-        try {
-            response.sendRedirect("/error/403");
         } catch (IOException e1) {
             e1.printStackTrace();
         }
