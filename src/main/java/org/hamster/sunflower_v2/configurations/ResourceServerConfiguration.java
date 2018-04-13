62f202021d9d5350b0028bcf6a6e5e38eddffeac
Czeide Dydriene Avanzado <czeide_avanzado@dlsu.edu.ph>
2018-04-13 14:36:34 +0800

    Admin is redirected to admin page

--- src/main/java/org/hamster/sunflower_v2/configurations/ResourceServerConfiguration.java
+++ src/main/java/org/hamster/sunflower_v2/configurations/ResourceServerConfiguration.java
@@ -6,19 +6,28 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by ONB-CZEIDE on 03/02/2018
@@ -78,25 +87,7 @@ public class ResourceServerConfiguration extends WebSecurityConfigurerAdapter {
 //                    }
 
                     response.sendRedirect("/login?error=" + error);
-                }).successHandler((request, response, authentication) -> {
-                    String username = request.getParameter("username");
-                    boolean isAdmin = false;
-
-                    response.setStatus(HttpServletResponse.SC_OK);
-                    userService.resetFailedAttempt(username);
-
-                    for (GrantedAuthority grantedAuthority : authentication.getAuthorities()) {
-                        if (grantedAuthority.getAuthority().equals("ADMIN")) {
-                            isAdmin = true;
-                        }
-                    }
-
-                    if (isAdmin) {
-                        response.sendRedirect("/admin");
-                    } else {
-                        response.sendRedirect("/");
-                    }
-                }).permitAll()
+                }).successHandler(new MySavedRequestAwareAuthenticationSuccessHandler()).permitAll()
                     .and()
                 .logout().logoutUrl("/logout").deleteCookies("JSESSIONID").invalidateHttpSession(true).permitAll()
                     .and()
@@ -132,5 +123,79 @@ public class ResourceServerConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    private class MySavedRequestAwareAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+        private RequestCache requestCache = new HttpSessionRequestCache();
+        private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+        public MySavedRequestAwareAuthenticationSuccessHandler() {
+            super();
+            setUseReferer(true);
+        }
+
+        @Override
+        public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
+            String username = request.getParameter("username");
+            StringBuilder url = new StringBuilder("https://localhost:8443/");
+            boolean isAdmin = false;
+
+            userService.resetFailedAttempt(username);
+
+            for (GrantedAuthority grantedAuthority : authentication.getAuthorities()) {
+                if (grantedAuthority.getAuthority().equals("ADMIN")) {
+                    isAdmin = true;
+                }
+            }
+
+            if (isAdmin) {
+                url.append("admin");
+            }
+
+            SavedRequest savedRequest = requestCache.getRequest(request, response);
+
+            if (savedRequest == null) {
+//                String targetUrl = determineTargetUrl(request, response);
+                String targetUrl = url.toString();
+
+                if (response.isCommitted()) {
+                    logger.debug("Response has already been committed. Unable to redirect to "
+                            + targetUrl);
+                    return;
+                }
+
+                redirectStrategy.sendRedirect(request, response, targetUrl);
+
+                super.clearAuthenticationAttributes(request);
+                return;
+            }
+
+            String targetUrlParameter = getTargetUrlParameter();
+            if (isAlwaysUseDefaultTargetUrl()
+                    || (targetUrlParameter != null && org.springframework.util.StringUtils.hasText(request
+                    .getParameter(targetUrlParameter)))) {
+                requestCache.removeRequest(request, response);
+//                String targetUrl = determineTargetUrl(request, response);
+                String targetUrl = url.toString();
+                if (response.isCommitted()) {
+                    logger.debug("Response has already been committed. Unable to redirect to "
+                            + targetUrl);
+                    return;
+                }
+
+                redirectStrategy.sendRedirect(request, response, targetUrl);
+                super.clearAuthenticationAttributes(request);
+
+                return;
+            }
+
+            clearAuthenticationAttributes(request);
+
+            // Use the DefaultSavedRequest URL
+            String targetUrl = savedRequest.getRedirectUrl();
+            logger.debug("Redirecting to DefaultSavedRequest Url: " + targetUrl);
+            getRedirectStrategy().sendRedirect(request, response, targetUrl);
+        }
     }
 }
